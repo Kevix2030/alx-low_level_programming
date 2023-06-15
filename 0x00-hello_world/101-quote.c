5ab3fc71e9109596a002356abf3f4dbdd5ac3438
kevix2030 <kevixx2030@gmail.com>
2023-06-15 15:14:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
-	return (1);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
+return (1);
 }
