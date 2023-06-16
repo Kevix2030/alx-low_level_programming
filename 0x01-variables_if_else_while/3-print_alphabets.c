c6ee72157dd0012c1dc01b082f5bee0427ee7cac
kevix2030 <kevixx2030@gmail.com>
2023-06-16 14:42:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * mai - Program entry point
+ * Return: Always return 0 (success)
+ */
+
+int main()
+{
+	char m;
+
+	char k;
+
+	m = 'a';
+	k = 'A';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	while
+		(k <= 'Z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
