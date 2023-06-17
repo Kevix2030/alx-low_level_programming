d990d579bc539cc353b0b716533f7f850d5d2469
kevix2030 <kevixx2030@gmail.com>
2023-06-16 17:34:11 -0700

    6-print_numberz.c

--- 0x00-hello_world/5-print_numbers.c
+++ 0x00-hello_world/5-print_numbers.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-
 /**
- *main - Entry point to the program.print numbers from 0 to 9.
- *
+ *main - Entry point to the program.
+ *Description: - the program will print numbers from 0 to 9.
+
  *Returns: Always 0 (success)
  */
 int main(void)
 {
-int num;
-for(num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
-return (0);
+	int num;
+
+
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
 }
- 
+
