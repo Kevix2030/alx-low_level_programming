f5c2c17b4259a03130791a24092bbf6e9a16b1e8
kevix2030 <kevixx2030@gmail.com>
2023-06-17 01:17:43 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * Main - prints all strings from 0 to 9
- * Return: Always 0 (success)
+ *Main - This is the main function of the program
+ *it prints numbers from 1 to 9
+*Return: 0 indicate successful execution.
  */
 int main(void)
 {
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,21 @@
 
 /**
  * main - Program entry point
- * Return:0 (success)
+ *
+ * Description: This function prints the numbers from 0 to 9 using putchar.
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
-{       
-        int i;
-        
-        for (i = '0'; i <= '9'; i++)
-                putchar(i);
-        putchar('\n');
-        return (0);
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	putchar((num % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
 }
