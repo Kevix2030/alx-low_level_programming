742fee581eae56bd431636c37a8ed6f2dd4b82f3
kevix2030 <kevixx2030@gmail.com>
2023-06-18 09:29:08 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,14 @@ int main(void)
 {
 	int num;
 	char m;
-	{
+
 	for (num = 0; num < 10; num++)
 	putchar((num % 10) + '0');
-	}
-	{
+
 	for (m = 'a'; m <= 'f'; m++)
-		putchar(m);
-	}
+	putchar(m);
 
 	putchar('\n');
 
-	Return 0;
+	Return(0);
 }
