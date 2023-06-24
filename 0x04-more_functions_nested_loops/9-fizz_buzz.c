ccf0eb81425a7bd3012ed0d6f2102719b216d46f
kevix2030 <kevixx2030@gmail.com>
2023-06-23 17:49:19 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,29 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (1 % 5 == 0))
+
+		if (i % 3 == 0 && 1 % 5 != 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (i % 5 == 0 && 1 % 3 != 0)
+		{
+			printf("Buzz")
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0)
+		else if (i == 1)
 		{
-			printf("Fizz ")
-		}
-		else if (i % 5 == 0)
+			printf("%d", i);
 		{
-			printf("Buzz ");
-		}
 		else
-		{
-		       	printf("%d", i);
+			print("%d", i);
 		}
 		printf("\n");
 
