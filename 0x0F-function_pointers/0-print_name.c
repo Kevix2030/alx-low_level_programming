d2f38381e146bf65dc049625fd20b0cd4f97edfa
kevix2030 <kevixx2030@gmail.com>
2023-07-20 03:07:24 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include "functions_pointers.h"
+#include "function_pointers.h"
 
 /**
- * print_name: prints names
- * @name: name of the person
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * Return: nothing
-*/
-
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+if (name && f)
+f(name);
 }
