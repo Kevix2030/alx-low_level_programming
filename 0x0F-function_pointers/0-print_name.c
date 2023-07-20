e658e5fb2f1ea801736aae3b1b7c7bd48f501687
kevix2030 <kevixx2030@gmail.com>
2023-07-20 03:04:51 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include "functions_pointers.h"
+
 /**
  * print_name: prints names
  * @name: name of the person
  *
  * Return: nothing
- */
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
